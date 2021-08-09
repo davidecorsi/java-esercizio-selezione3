89370b028a1ba8e91d624c86861b1af6ca71395c
Davide Corsi <davide.corsi@par-tec.it>
2021-08-09 14:18:33 +0200

    soluzione

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,25 @@ public class Main {
 		String firstPlayer = input.nextLine();
 		System.out.print("Giocatore 2: ");
 		String secondPlayer = input.nextLine();
+		
+		// Controllo che gli input siano corretti
+		if((!firstPlayer.equals("carta") && !firstPlayer.equals("forbice") && !firstPlayer.equals("sasso")) ||
+				(!secondPlayer.equals("carta") && !secondPlayer.equals("forbice") && !secondPlayer.equals("sasso"))) {
+			System.out.println("Input inserito non valido");
+		} else if(firstPlayer.equals(secondPlayer)) {
+			System.out.println("Parità");
+		} else if(firstPlayer.equals("carta") && secondPlayer.equals("sasso")) {
+			System.out.println("Vince il giocatore 1");
+		} else if(firstPlayer.equals("carta") && secondPlayer.equals("forbice")) {
+			System.out.println("Vince il giocatore 2");
+		} else if(firstPlayer.equals("forbice") && secondPlayer.equals("carta")) {
+		System.out.println("Vince il giocatore 1");
+		} else if (firstPlayer.equals("forbice") && secondPlayer.equals("sasso")) {
+			System.out.println("Vince il giocatore 2");
+		} else if(firstPlayer.equals("sasso") && secondPlayer.equals("forbice")) {
+			System.out.println("Vince il giocatore 1");
+		} else if (firstPlayer.equals("sasso") && secondPlayer.equals("carta")) {
+			System.out.println("Vince il giocatore 2");
+		}
 	}
 }
